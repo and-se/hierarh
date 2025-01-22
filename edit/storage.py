33cc289fd5129ed6a8dd02b21d8fa8649100ee32
and-se <andse-t610@yandex.ru>
2025-01-22 19:26:56 +0300

    Поддержка в БД и тесты сохранения версий статей.
    Сейчас последний писатель всегда успешно пишет свои данные, даже если отталкивался от устаревшего текста.
    Потом по истории версий можно вручную разобраться...

--- edit/storage.py
+++ edit/storage.py
@@ -2,9 +2,10 @@ import re
 import json
 from db import get_db, orm_all_words_search_condition
 import settings
-from peewee import Model, AutoField, TextField, BooleanField
+from peewee import Model, AutoField, TextField, BooleanField, IntegerField
 
 DbName = settings.EditDbName
+EditDb = None  # БД редактирования, инициализируется ниже
 
 class HierarhEditStorage:
     def __init__(self):
@@ -20,34 +21,34 @@ class TextCollectionDb:
         self.name = name
         self.orm = orm_model
 
-    def portion(self, skip=0, take=20, query=None):
-        q = self.orm.select(self.orm.id, self.orm.html, self.orm.reg_data) \
-                    .where(orm_all_words_search_condition(query, self.orm.header)) \
-                    .order_by(self.orm.header) \
-                    .limit(take).offset(skip)
-        return [x.toTextCafedra() for x in q]
-
     def new(self):
         doc = TextCafedra()
         return doc
 
-    def upsert(self, doc: 'TextCafedra', reg_data:dict=None) -> 'TextCafedra':
+    def upsert(self, doc: 'TextCafedra', reg_data:dict=None, fix_reg_data=True) -> 'TextCafedra':
         # create new or update current item
         if reg_data is not None:
             if not isinstance(reg_data, dict):
                 raise ValueError("reg_data must be dict or None")
         else:
-            reg_data = doc.reg_data
+            reg_data = doc.reg_data or {}
 
-        if 'when' not in reg_data:
+        if fix_reg_data:
             from time import time as unix_now
             reg_data['when'] = unix_now()
 
         rgd = json.dumps(reg_data)
         if doc.key:
-            doc.key = int(doc.key)
-            key2 = self.orm.replace(id=doc.key, header=doc.header(), html=doc.html, reg_data=rgd).execute()
-            assert doc.key==key2
+            with EditDb.atomic(lock_type = 'IMMEDIATE'):
+                doc.key = int(doc.key)
+                old = self.orm.get(doc.key)
+                if old:
+                    num = self.version_count(doc.key) or 0
+                    hist = old.toVersionOrm(coll_name = self.name, num=num + 1)
+                    hist.save()
+
+                key2 = self.orm.replace(id=doc.key, header=doc.header(), html=doc.html, reg_data=rgd).execute()
+                assert doc.key==key2
         else:
             doc.key = self.orm.create(header=doc.header(), html=doc.html, reg_data=rgd).id
 
@@ -60,6 +61,33 @@ class TextCollectionDb:
         r = self.orm.get_or_none(key)
         if r:
             return r.toTextCafedra()
+
+    def _portion_query(self, query):
+        return self.orm.select() \
+                    .where(orm_all_words_search_condition(query, self.orm.header)) \
+
+    def portion(self, skip=0, take=20, query=None):
+        q = self._portion_query(query) \
+                    .order_by(self.orm.header) \
+                    .limit(take).offset(skip)
+        return [x.toTextCafedra() for x in q]
+
+    def count(self, query=None):
+        return self._portion_query(query).count()
+
+    def _version_query(self, key):
+        return VersionOrm.select().where((VersionOrm.doc_key == key) & \
+                                      (VersionOrm.collection == self.name))
+
+    def versions(self, key, skip=0, take=20):
+        q = self._version_query(key) \
+                      .order_by(VersionOrm.num, VersionOrm.id) \
+                      .limit(take).offset(skip)
+        return [x.toTextVersion() for x in q]
+
+    def version_count(self, key):
+        return self._version_query(key).count()
+
 
 
 class TextCafedra:
@@ -96,6 +124,25 @@ class TextCafedra:
     def __str__(self):
         return repr(self)
 
+class TextVersion:
+    def __init__(self, coll, key, html, reg_data: dict):
+        self.collection = coll
+        self.key = key
+        self.html = html
+
+        if not reg_data:
+            reg_data = {}
+        if not isinstance(reg_data, dict):
+            raise ValueError('reg_data must be dict')
+        self.reg_data = reg_data
+
+    def __repr__(self):
+        return f"TextVersion({self.collection} {self.key} {self.reg_data.get('when')})"
+
+    def __str__(self):
+        return repr(self)
+
+
 
 ### ORM ###
 
@@ -105,22 +152,46 @@ class CafedraEditOrm(Model):
 
     id = AutoField()
     header = TextField(index=True)
-    html = TextField(null=False)
-    reg_data = TextField(null=True)
+    html = TextField()
+    reg_data = TextField()
 
     def toTextCafedra(self):
         r = TextCafedra.from_html(self.id, self.html)
         r.reg_data = json.loads(self.reg_data)
+        assert isinstance(r.reg_data, dict)
         return r
 
+    def toVersionOrm(self, coll_name, num: int):
+        return VersionOrm(collection = coll_name, doc_key = self.id, \
+                          doc_data = self.html, \
+                          doc_reg_data = self.reg_data,
+                          num = num)
 
-EditDb = None
+class VersionOrm(Model):
+    class Meta:
+        table_name = 'TextHistory'
+
+    id = AutoField()
+    collection = TextField()
+    doc_key = IntegerField()
+    doc_data = TextField()
+    doc_reg_data = TextField()
+
+    num = IntegerField()
+
+    def toTextVersion(self):
+        return TextVersion(self.collection, self.doc_key, \
+                           self.doc_data, json.loads(self.doc_reg_data))
+
+
+VersionOrm.add_index(VersionOrm.collection, VersionOrm.doc_key)
+
 
 def init_edit_db():
     global EditDb
     EditDb = get_db(settings.EditDbName)
-    EditDb.bind([CafedraEditOrm])
-    EditDb.create_tables([CafedraEditOrm])
+    EditDb.bind([CafedraEditOrm, VersionOrm])
+    EditDb.create_tables([CafedraEditOrm, VersionOrm])
     return EditDb
 
 init_edit_db()
--- edit/test.py
+++ edit/test.py
@@ -64,13 +64,14 @@ class TestStorage(unittest.TestCase):
         n4 = mn[0]
         self.checkCafEqual(n2, n4)
 
-        # Модифицировали html, но не трогали reg_data.when
-        # --> оно не изменилось
+        # Update
         n4.html = load_file('1-div.html').html
+        old_when = n4.reg_data['when']
         n5 = self.cafedra.upsert(n4)
         self.assertTrue(id(n5) == id(n4))
+        self.assertGreater(n5.reg_data['when'], old_when)
 
-        # Новый объект без reg_data -> when изменился
+        ## Новый объект без reg_data
         n6 = load_file('1.html')
         n6.key = n5.key
         self.cafedra.upsert(n6)
@@ -81,9 +82,61 @@ class TestStorage(unittest.TestCase):
         n8 = self.cafedra.portion(take=1)[0]
         self.checkCafEqual(n8, n6)
 
+        # Откатываем документ к начальному состоянию
+        # Используем режим, когда reg_data записываются какие есть,
+        # а не исправляются системой, в частности
+        # reg_data[when] должен откатиться назад
+        self.cafedra.upsert(n2, fix_reg_data=False)
+        self.checkCafEqual(n2, n3)
+        self.assertEqual(n2.reg_data['when'], n3.reg_data['when'])
+        self.assertGreater(n8.reg_data['when'], n2.reg_data['when'])
+
         # test no unknown doc
         no = self.cafedra.get(12345)
         self.assertIsNone(no)
+
+    def test_versions(self):
+
+        def checkCafVerEqual(caf, ver):
+            self.assertEqual(ver.collection, 'cafedra')
+            self.assertEqual(ver.key, caf.key)
+            self.assertEqual(ver.reg_data, caf.reg_data)
+            self.assertEqual(ver.html, caf.html)
+
+        n1 = load_file('1.html')
+        self.cafedra.upsert(n1)
+
+        v = self.cafedra.versions(n1.key)
+        self.assertEqual(len(v), 0)
+
+        n2 = load_file('1-div.html')
+        n2.reg_data['who'] = 'editor'
+        n2.key = n1.key
+        self.cafedra.upsert(n2)
+
+        v = self.cafedra.versions(n1.key)
+
+        self.assertEqual(len(v), 1)
+        checkCafVerEqual(n1, v[0])
+
+        n3 = self.cafedra.get(n2.key)
+        n3.html += 'UPDATED3'
+        n3.reg_data['who'] = 'admin'
+        n3.reg_data['comment'] = 'restore db'
+        self.cafedra.upsert(n3, fix_reg_data=False)
+
+        v = self.cafedra.versions(n1.key)
+        self.assertEqual(len(v), 2)
+        checkCafVerEqual(n2, v[-1])
+
+        old_when = n1.reg_data['when']
+        self.cafedra.upsert(n1)
+        v = self.cafedra.versions(n3.key)
+        self.assertEqual(len(v), 3)
+        checkCafVerEqual(n3, v[-1])
+
+        self.assertGreater(n1.reg_data['when'], old_when)
+
 
     def checkCafEqual(self, c1, c2):
         self.assertEqual(c1.key, c2.key)
