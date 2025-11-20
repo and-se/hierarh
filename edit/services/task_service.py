525bf20fee5ac51399e7ea62c607ee917ec3c4bd
and <andse-t610@yandex.ru>
2025-11-20 14:13:50 +0300

    Отдельный статус для задач, у которых есть только комментарий

--- edit/services/task_service.py
+++ edit/services/task_service.py
@@ -53,11 +53,13 @@ class TaskService:
         t.set_raw_answer(answer_json)
         problem_count, resolved_count = t.check_problem_resolve_status()
 
-        if resolved_count > 0 or answer_json.get('comment'):
+        if resolved_count > 0:
             if problem_count <= resolved_count:
                 new_status = "обработано"
             else:
                 new_status = "в процессе"
+        elif answer_json.get('comment'):
+            new_status = "прокомментирована"
         else:
             new_status = 'новая'
             
