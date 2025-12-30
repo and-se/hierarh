902016b8ddb9f94d6b6c3f2836a87019af55d23d
and <andse-t610@yandex.ru>
2025-12-31 00:31:35 +0300

    Рефакторинг: потсроитель запросов в индексе епископов (builder pattern)

--- edit/index.py
+++ edit/index.py
@@ -59,92 +59,41 @@ class EpiskopIndex:
                        begin_year=None, end_year=None,
                        cafedra: str | None = None,
                        saint_title=None,
-                       is_obn: bool | None = None) -> list['EpiskopIndexOrm']:
-        OrmModel = self.orm_model
+                       is_obn: bool | None = None) -> list[tuple]:
         if not name and not surname:
             return []
         if not name:
             raise ValueError('name must be not empty!')
         if name == 'NN' and not surname:
             return []  # NN is unknown man, so two NNs are different
-        #cond = fn.LOWER_PY(EpiskopIndexOrm.name) == name.lower()  # точное совпадение
-        cond = fn.INSTR(fn.LOWER_PY(OrmModel.name), name.lower())  # по подстроке
-        # cond = fn.INSTR(fn.LOWER_PY(EpiskopIndexOrm.name), name.lower()) == 1 # с начала строки
-        if surname:
-            #cond = cond & (fn.LOWER_PY(EpiskopIndexOrm.surname) == surname.lower())  # точное совпадение
-            cond = cond & (fn.INSTR(fn.LOWER_PY(OrmModel.surname), surname.lower()))  # по подстроке
-            #cond = cond & (fn.INSTR(fn.LOWER_PY(EpiskopIndexOrm.surname), surname.lower()) == 1) # с начала строки
-        else:
-            cond = cond & OrmModel.surname.is_null()
-
-        if is_obn is not None:
-            cond = cond & (OrmModel.is_obn == is_obn)
         
-        if saint_title:
-            cond = cond & (fn.LOWER_PY(OrmModel.saint_title) == fn.LOWER_PY(saint_title.strip()))
+        query = self.get_builder() \
+                    .where_name(name, surname) \
+                    .where_obn(is_obn) \
+                    .where_saint_title(saint_title) \
+                    .where_years(begin_year, end_year) \
+                    .where_cafedra(cafedra) \
+                    .limit(10)
 
-        if begin_year or end_year:
-            if not begin_year:
-                begin_year = end_year
-            elif not end_year:
-                end_year = begin_year
-            
-            if begin_year > end_year:
-                raise ValueError("begin_year must be <= end_year")
-            
-            # OLD - фактически качество поиска только ухудшало
-            # разрешаем зазор в 5 лет от означенного интервала
-            # кроме приблизительного совпадения это позволяет
-            # обработать интервалы в индексе, где начало=конец
-            #begin_year -= 5
-            #end_year += 5
-
-            cond = cond & (
-                # BAD: если в индексе у епископа лет нет, просто берём его в результат
-                #(EpiskopIndexOrm.min_year.is_null() & EpiskopIndexOrm.max_year.is_null()) |
-
-                # требуем пересечение между запрошенным отрезком лет и годами епископа в индексе
-                # если у епископа нету лет, он не попадёт в результат
-                ((OrmModel.min_year <= end_year) & (OrmModel.max_year >= begin_year))
-                )
-
-        if cafedra:
-            cond = cond & (fn.INSTR(fn.LOWER_PY(OrmModel.cafedras), cafedra.lower()))
-
-        ep_qq = OrmModel.select().where(cond).limit(10).namedtuples()
-
-        return list(ep_qq)
+        return list(query.run())
     
-        #from storage import EditDb
-        #print(ep_qq, "PLAN:", EditDb.execute_sql(f'EXPLAIN QUERY PLAN {ep_qq}').fetchall(), '\n\n\n')
-        #raise ValueError()
-
-        #res = list(ep_qq)
-        #if cafedra:
-        #    res = [c for c in res if cafedra.lower() in c.cafedras.lower()]
-
-        #return res[:10]
-    
+    def get_builder(self):
+        return EpiskopQueryBuilder(self.orm_model)
+        
     def find_by_header(self, query, clear_input=True):
         """
         Поиск по заголовку на основе вхождения слов из запроса
 
         query - что ищем
         clear_input - очистить входной запрос от обычно мешающих поиску слов и знаков препинания
-        - сейчас остаются только слова с большой буквы, за исключением римским номеров и чинов святости.
+        - сейчас остаются только слова с большой буквы, за исключением римских номеров и чинов святости.
         """
-        OrmModel = self.orm_model
-        if clear_input:
-            # Оставляем только слова с большой буквы (и исключением некоторых)
-            words = [x for x in re.split('[^а-яёa-z-]+', query, flags=re.I) 
-                    if x and x[0].isupper() and x not in ('I', 'II', 'III', 'IV', 'V', 'Святой', 'Святитель')]
-            query = ' '.join(words)
-
-        r = OrmModel.select() \
-            .where(orm_all_words_search_condition(query, OrmModel.header)) \
-            .order_by(OrmModel.header).limit(10).namedtuples()
+        q = self.get_builder() \
+            .where_header(query, clear_input) \
+            .order_by(self.orm_model.header) \
+            .limit(10)
         
-        return r
+        return list(q.run())
 
     def rebuild(self):
         self.log.info("Start rebuild episkop index")
@@ -205,6 +154,114 @@ class EpiskopIndex:
             
         self.log.info(f"Episkop index built. Total records {i}")
 
+class EpiskopQueryBuilder:
+    def __init__(self, orm: 'EpiskopIndexOrm'):
+        self.orm = orm
+        self.cond = True
+        self._offset = None
+        self._limit = None
+        self._sort = None
+
+    def append_cond(self, condition):
+        self.cond = self.cond & condition
+
+    
+    def where_name(self, name, surname=None) -> 'EpiskopQueryBuilder':
+        """
+        Поиск по имени и фамилии
+
+        Если имя не задано, оно не учитывается.
+        Если фамилия не задана, ищуется записи с пустой фамилией
+        """
+        if not name and not surname:
+            raise ValueError("Name and surname can't be None together!")
+        
+        if name:
+            self.append_cond(fn.INSTR(fn.LOWER_PY(self.orm.name), name.lower()))
+
+        if surname:
+            self.append_cond(fn.INSTR(fn.LOWER_PY(self.orm.surname), surname.lower()))            
+        else:
+            self.append_cond(self.orm.surname.is_null())
+        
+        return self
+    
+    def where_obn(self, is_obn: bool | None):
+        """
+        Фильтр по признаку обновленчества
+        """
+        if is_obn is not None:
+            self.append_cond(self.orm.is_obn == is_obn)
+        return self
+
+    def where_saint_title(self, saint_title):
+        if saint_title:
+            self.append_cond(fn.LOWER_PY(self.orm.saint_title) == fn.LOWER_PY(saint_title.strip()))
+        return self
+    
+    def where_cafedra(self, cafedra):
+        if cafedra:
+            self.append_cond(fn.INSTR(fn.LOWER_PY(self.orm.cafedras), cafedra.lower()))
+        return self
+
+    def where_years(self, begin_year, end_year):
+        if not begin_year and not end_year:
+            return self
+        
+        if not begin_year:
+            begin_year = end_year
+        elif not end_year:
+            end_year = begin_year
+        
+        if begin_year > end_year:
+            raise ValueError("begin_year must be <= end_year")
+        
+        self.append_cond(                
+                # требуем пересечение между запрошенным отрезком лет и годами епископа в индексе
+                # если у епископа нету лет, он не попадёт в результат
+                ((self.orm.min_year <= end_year) & (self.orm.max_year >= begin_year))
+        )
+        return self
+    
+    def where_header(self, query, clear_input=True):
+        if clear_input:
+            # Оставляем только слова с большой буквы (за исключением некоторых)
+            words = [x for x in re.split('[^а-яёa-z-]+', query, flags=re.I) 
+                    if x and x[0].isupper() and x not in ('I', 'II', 'III', 'IV', 'V', 'Святой', 'Святитель')]
+            query = ' '.join(words)
+        
+        if query:
+            self.append_cond(orm_all_words_search_condition(query, self.orm.header))
+
+        return self
+
+    def order_by(self, *columns):
+        self._sort = columns
+        return self
+
+    def offset(self, skip):
+        self._offset = skip
+        return self
+
+    def limit(self, count):
+        self._limit = count
+        return self
+
+    def run(self):
+        q = self.orm.select().where(self.cond)
+        if self.offset:
+            q = q.offset(self._offset)
+        if self.limit:
+            q = q.limit(self._limit)
+        
+        if self._sort:
+            q.order_by(*self._sort)
+        
+        #from storage import EditDb
+        #print(q, "PLAN:", EditDb.execute_sql(f'EXPLAIN QUERY PLAN {q}').fetchall(), '\n\n\n')
+        #raise ValueError()
+
+        return q.namedtuples()
 
 
 class EpiskopIndexOrm(Model):
